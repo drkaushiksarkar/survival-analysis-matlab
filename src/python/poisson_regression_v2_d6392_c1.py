14e3d267e5682c643f23a9b795e8943cb85a50b7
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2012-02-10 10:17:14 +0000

    fix(python): poisson_regression v2

---
+++ src/python/poisson_regression_v2_d6392_c1.py
@@ -0,0 +1,27 @@
+"""Tests for poisson_regression v2_d6392."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestPoissonRegression_v2_d6392(unittest.TestCase):
+    def test_model_fit(self):
+        X = np.random.randn(100, 4)
+        y = X @ np.random.randn(4) + np.random.randn(100) * 0.5
+        beta = np.linalg.lstsq(X, y, rcond=None)[0]
+        self.assertEqual(len(beta), 4)
+
+    def test_bootstrap_ci(self):
+        sample = np.random.exponential(2, 500)
+        boots = [np.mean(np.random.choice(sample, len(sample))) for _ in range(1000)]
+        ci = np.percentile(boots, [2.5, 97.5])
+        self.assertLess(ci[0], ci[1])
+
+    def test_spatial_correlation(self):
+        coords = np.random.uniform(0, 100, (50, 2))
+        values = np.random.randn(50)
+        self.assertEqual(len(coords), len(values))
+
+
+if __name__ == "__main__":
+    unittest.main()
