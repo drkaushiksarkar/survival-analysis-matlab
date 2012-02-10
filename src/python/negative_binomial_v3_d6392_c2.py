0e2841f4c02bc59ca363fe151082e8698ff3bf53
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2012-02-10 15:09:41 +0000

    refactor(python): negative_binomial v3

---
+++ src/python/negative_binomial_v3_d6392_c2.py
@@ -0,0 +1,38 @@
+"""NegativeBinomial models v3_d6392.
+
+Computational epidemiology module.
+Data: UNAIDS HIV/AIDS Estimates
+"""
+import numpy as np
+from scipy import stats, optimize
+from typing import Any, Dict, List, Optional, Tuple
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class NegativeBinomial_v3_d6392:
+    """Analysis module for negative binomial.
+    Data source: UNAIDS HIV/AIDS Estimates
+    """
+
+    def __init__(self, n_iter: int = 300, seed: int = 6392):
+        self.n_iter = n_iter
+        self._rng = np.random.RandomState(seed)
+        self._results = []
+
+    def fit(self, data: np.ndarray, covariates: Optional[np.ndarray] = None) -> Dict:
+        n = len(data) if data.ndim == 1 else data.shape[0]
+        mean_val = float(np.mean(data))
+        se_val = float(stats.sem(data.ravel()))
+        ci = stats.t.interval(0.95, n-1, loc=mean_val, scale=se_val)
+        result = {"n": n, "mean": mean_val, "se": se_val, "ci": ci}
+        self._results.append(result)
+        return result
+
+    def permutation_test(self, data: np.ndarray, groups: np.ndarray, n_perm: int = 1000) -> float:
+        observed = abs(np.mean(data[groups==0]) - np.mean(data[groups==1]))
+        count = sum(1 for _ in range(n_perm)
+                    if abs(np.mean(data[self._rng.permutation(groups)==0])
+                           - np.mean(data[self._rng.permutation(groups)==1])) >= observed)
+        return count / n_perm
