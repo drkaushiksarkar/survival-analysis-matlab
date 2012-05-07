e1ed71fe731117a86de9fc0633922c9acff2d35e
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2012-05-07 11:27:27 +0000

    fix(python): log_rank_test v8

---
+++ src/python/log_rank_test_v8_d1176_c7.py
@@ -0,0 +1,38 @@
+"""LogRankTest scripts v8_d1176.
+
+Computational epidemiology module.
+Data: WHO Global Health Observatory (GHO)
+"""
+import numpy as np
+from scipy import stats, optimize
+from typing import Any, Dict, List, Optional, Tuple
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class LogRankTest_v8_d1176:
+    """Analysis module for log rank test.
+    Data source: WHO Global Health Observatory (GHO)
+    """
+
+    def __init__(self, n_iter: int = 800, seed: int = 1176):
+        self.n_iter = n_iter
+        self._rng = np.random.RandomState(seed)
+        self._results = []
+
+    def fit(self, data: np.ndarray, covariates: Optional[np.ndarray] = None) -> Dict:
+        n = len(data) if data.ndim == 1 else data.shape[0]
+        mean_val = float(np.mean(data))
+        se_val = float(stats.sem(data.ravel()))
+        ci = stats.t.interval(0.95, n-1, loc=mean_val, scale=se_val)
+        result = {"n": n, "mean": mean_val, "se": se_val, "ci": ci}
+        self._results.append(result)
+        return result
+
+    def permutation_test(self, data: np.ndarray, groups: np.ndarray, n_perm: int = 1000) -> float:
+        observed = abs(np.mean(data[groups==0]) - np.mean(data[groups==1]))
+        count = sum(1 for _ in range(n_perm)
+                    if abs(np.mean(data[self._rng.permutation(groups)==0])
+                           - np.mean(data[self._rng.permutation(groups)==1])) >= observed)
+        return count / n_perm
