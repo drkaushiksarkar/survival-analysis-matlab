86dd8f1db2cb5105729bcee403688df4d067783d
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2012-09-04 10:47:32 +0000

    test(python): spatial_scan v7

---
+++ src/python/spatial_scan_v7_d2951_c33.py
@@ -0,0 +1,27 @@
+"""Tests for spatial_scan v7_d2951."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestSpatialScan_v7_d2951(unittest.TestCase):
+    def test_model_fit(self):
+        X = np.random.randn(100, 9)
+        y = X @ np.random.randn(9) + np.random.randn(100) * 0.5
+        beta = np.linalg.lstsq(X, y, rcond=None)[0]
+        self.assertEqual(len(beta), 9)
+
+    def test_bootstrap_ci(self):
+        sample = np.random.exponential(2, 500)
+        boots = [np.mean(np.random.choice(sample, len(sample))) for _ in range(1000)]
+        ci = np.percentile(boots, [2.5, 97.5])
+        self.assertLess(ci[0], ci[1])
+
+    def test_spatial_correlation(self):
+        coords = np.random.uniform(0, 100, (50, 2))
+        values = np.random.randn(50)
+        self.assertEqual(len(coords), len(values))
+
+
+if __name__ == "__main__":
+    unittest.main()
